f7cadc03f8bb9b72df056463ab826a6589000ed6
Ansh Sharma <122905517+AnshSharma16@users.noreply.github.com>
2026-02-17 16:41:16 +0530

    Update validation_schema.py

--- 01-data-engineering-foundations/11_ci_cd/src/validation_schema.py
+++ 01-data-engineering-foundations/11_ci_cd/src/validation_schema.py
@@ -1,8 +1,8 @@
-import pandera as pa
+import pandera.pandas as pa
 from pandera import Column, Check
 
 sales_schema = pa.DataFrameSchema({
     "Units Sold": Column(int, Check.ge(0)),
-    "Unit Price": Column(float, Check.ge(0)),
+    "Unit Price": Column(float, Check.ge(0), coerce=True),
     "Sales Channel": Column(str, Check.isin(["Online", "Offline"]))
 })
