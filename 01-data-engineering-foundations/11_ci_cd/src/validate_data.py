69a70ed398ccb1b4dd11dbdc46403825d9db1680
Ansh Sharma <122905517+AnshSharma16@users.noreply.github.com>
2026-02-17 16:38:17 +0530

    Update validate_data.py

--- 01-data-engineering-foundations/11_ci_cd/src/validate_data.py
+++ 01-data-engineering-foundations/11_ci_cd/src/validate_data.py
@@ -2,8 +2,7 @@ import pandas as pd
 from validation_schema import sales_schema
 
 def run_validation():
-    df = pd.read_csv("01-data-engineering-foundations/11_ci_cd/data/sample.csv")
-)
+    df = pd.read_csv("data/sample.csv")
     sales_schema.validate(df)
     print("✅ Data validation passed")
 
